086833f1b2d1bfa43a3aaa7995f735889c8d7cee
fancy <fancy_the_lion@proton.me>
2023-12-25 12:56:25 +0100

    Able to see timestamp and temperature

--- fake_weather_frontend/src/components/CurrentWeatherComponent.vue
+++ fake_weather_frontend/src/components/CurrentWeatherComponent.vue
@@ -1,7 +1,45 @@
 <script setup>
+  import {onMounted, ref} from "vue";
+  import moment from "moment";
+
+  const apiBaseUrl = process.env.VUE_APP_API_URL
+
+  const currentTemperature = ref(0)
+
+  const currentWeatherTime = ref(0)
+
+  // OnMounted hook
+  onMounted(async () =>
+  {
+    await OnLoad();
+  })
+
+  // Called when page is loaded
+  async function OnLoad()
+  {
+    const currentWeatherResponse = await (await fetch(apiBaseUrl + "/api/Weather/Current", {
+      method: 'GET'
+    })).json()
+
+    currentWeatherTime.value = moment(currentWeatherResponse
+        .currentWeather
+        .timestamp).format('HH:mm:ss DD.MM.YYYY')
+
+    currentTemperature.value = currentWeatherResponse
+        .currentWeather
+        .temperature
+  }
 
 </script>
 
 <template>
-  <h1>Привет, погода!</h1>
+  <h1 class="centered">Привет, погода!</h1>
+
+  <div>
+    Погода на время: {{ currentWeatherTime }}
+  </div>
+
+  <div>
+    Сейчас {{ currentTemperature }} градусов.
+  </div>
 </template>
