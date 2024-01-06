1647ec86716744ea1c79c31c345b09d35a298b10
fancy <fancy_the_lion@proton.me>
2024-01-06 11:18:09 +0100

    Displayed time and temperature

--- FakeWeatherBackend/FakeWeatherBackend/obj/Debug/net7.0/FakeWeatherBackend.AssemblyInfo.cs
+++ FakeWeatherBackend/FakeWeatherBackend/obj/Debug/net7.0/FakeWeatherBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FakeWeatherBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf6f898a617949c7948f4b148a0b377add7044f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19c6b42bcf3227bd00027eb0c8bbaa275f8f2840")]
 [assembly: System.Reflection.AssemblyProductAttribute("FakeWeatherBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FakeWeatherBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- fake_weather_frontend/src/components/CurrentWeatherComponent.vue
+++ fake_weather_frontend/src/components/CurrentWeatherComponent.vue
@@ -1,12 +1,11 @@
 <script setup>
-  import {onMounted, ref} from "vue";
-  import moment from "moment";
+import {onMounted, ref} from "vue";
+import moment from "moment";
 
   const apiBaseUrl = process.env.VUE_APP_API_URL
 
-  const currentTemperature = ref(0)
-
-  const currentWeatherTime = ref(0)
+  const lastWeatherTime = ref(0)
+  const lastWeatherTemperature = ref(0)
 
   // OnMounted hook
   onMounted(async () =>
@@ -17,16 +16,24 @@
   // Called when page is loaded
   async function OnLoad()
   {
-    const currentWeatherResponse = await (await fetch(apiBaseUrl + "/api/Weather/Current", {
+    const lastWeatherReferenceResponse = await (await fetch(apiBaseUrl + "/api/WeatherReference/Last", {
       method: 'GET'
     })).json()
 
-    currentWeatherTime.value = moment(currentWeatherResponse
-        .currentWeather
+    const lastWeatherReference = lastWeatherReferenceResponse.weatherReference.weatherId
+
+    const lastWeatherResponse = await (await fetch(apiBaseUrl + "/api/Weather/" + lastWeatherReference, {
+      method: 'GET'
+    })).json()
+
+    console.log(lastWeatherResponse)
+
+    lastWeatherTime.value = moment(lastWeatherResponse
+        .weather
         .timestamp).format('HH:mm:ss DD.MM.YYYY')
 
-    currentTemperature.value = currentWeatherResponse
-        .currentWeather
+    lastWeatherTemperature.value = lastWeatherResponse
+        .weather
         .temperature
   }
 
@@ -34,12 +41,9 @@
 
 <template>
   <h1 class="centered">Привет, погода!</h1>
+  <h2 class="centered">Самая свежая (из известных) погода:</h2>
+  <p class="centered">Время: {{ lastWeatherTime }}</p>
+  <p class="centered">Температура: {{ lastWeatherTemperature }}</p>
 
-  <div>
-    Погода на время: {{ currentWeatherTime }}
-  </div>
 
-  <div>
-    Сейчас {{ currentTemperature }} градусов.
-  </div>
 </template>
