6e524cee13d31bb6e3b0fa7ecfc26070d81793dc
fancy <fancy_the_lion@proton.me>
2024-01-14 14:52:04 +0100

    - Added weather icons

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # LessonFakeWeather
 First ASP.NET Core lesson
+
+# Icons
+Taken from https://www.iconsdb.com, CC-BY-ND 3.0
--- fake_weather_frontend/src/components/CloudinessComponent.vue
+++ fake_weather_frontend/src/components/CloudinessComponent.vue
@@ -1,13 +1,41 @@
 <script setup>
-  import {defineProps} from "vue";
+import {defineProps, ref} from "vue";
+
+  const iconsBaseAddress = "images/"
 
   const props = defineProps({
     cloudiness: Number
   })
 
+  const iconAddress = ref("")
+
+  if (props.cloudiness < 0)
+  {
+    throw new Error("Cloudiness must be at least 0!")
+  }
+  else if (props.cloudiness >= 0 && props.cloudiness < 30)
+  {
+    iconAddress.value = iconsBaseAddress + "cloudiness-sunny.webp"
+  }
+  else if (props.cloudiness >= 30 && props.cloudiness < 80)
+  {
+    iconAddress.value = iconsBaseAddress + "cloudiness-partly.webp"
+  }
+  else if (props.cloudiness >= 80 && props.cloudiness <= 100)
+  {
+    iconAddress.value = iconsBaseAddress + "cloudiness-full.webp"
+  }
+  else
+  {
+    throw new Error("Cloudiness must be less than 100!");
+  }
 </script>
 
 <template>
+  <div class="centered">
+    <img class="cloudiness-icon" :src="iconAddress" alt="Cloudiness icon" />
+  </div>
+
   <div>Облачность: {{ props.cloudiness }}</div>
 </template>
 
